3492464eb53c1392ce54e107b0f64e7ac045b37c
LordGuafa <126006143+LordGuafa@users.noreply.github.com>
2024-09-16 13:00:10 -0500

    Comentarios

---
+++ Ejemplo MVC/Ejercicio/ejemplo MVC/src/view/Vista.java
@@ -0,0 +1,27 @@
+package view;
+
+import java.util.Scanner;
+
+import controller.Control;
+
+public class Vista {
+    // Para utilizar la inyección de dependencias se requiere una variable de tipo
+    // Control que se utilizará en el constructor
+    Control control;
+
+    public Vista(Control control) {
+        this.control = control;
+    }
+
+    // Método para mostrar por consola los mensajes del programa
+    public void mostrar(String mensaje) {
+        System.out.println(mensaje);
+    }
+
+    // Método para capturar los datos introducidos por teclado
+    public String capturar() {
+        Scanner sc = new Scanner(System.in);
+        String mensaje = sc.nextLine();
+        return mensaje;
+    }
+}
